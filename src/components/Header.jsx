34f1481f908df4489e5d4b5a00f8d52f27b422bf
Carlos <carlosmanuelibarra11@gmail>
2023-05-12 23:32:33 -0500

    agregeue solo un cambio en el h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ function Header(){
 
     return(
         <>
-            <h1 className="text-5xl font-black uppercase text-center md:w-2/3 mx-auto">Seguimiento Pacientes <span className="text-indigo-600">Veterinaria</span></h1>
+            <h1 className="text-5xl font-black uppercase text-center md:w-2/3 mx-auto">Seguimiento Pacientes {""} con GitHub {""} 
+            <span className="text-indigo-600">Veterinaria</span></h1>
         </>
     )
 }
